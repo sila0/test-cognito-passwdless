e52d6e856079f0d5844c8b8239a2594611df310b
sila <taepakdees@gmail.com>
2021-01-26 21:46:27 +0700

    revise createAuthChallent

--- lambda/createAuthChallenge/createAuthChallenge.go
+++ lambda/createAuthChallenge/createAuthChallenge.go
@@ -11,30 +11,9 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ses"
 )
 
-// Email -
-type Email struct {
-	Name      string
-	Sender    string
-	Recipient string
-	Subject   string
-	TextBody  string
-	HTMLBody  string
-	PDFFile   string
-	PDFBase64 string
-	MIME      string
-}
-
 const (
-	// Sender - Replace sender@example.com with your "From" address.
-	// This address must be verified with Amazon SES.
-	Sender = "sila@colonandcurve.co.th"
-
 	// SesRegion - Replace us-west-2 with the AWS Region you're using for Amazon SES.
 	SesRegion = "ap-south-1"
 
@@ -51,7 +30,8 @@ const (
 // HandleLambdaEvent -
 func HandleLambdaEvent(ctx context.Context, event *events.CognitoEventUserPoolsCreateAuthChallenge) (*events.CognitoEventUserPoolsCreateAuthChallenge, error) {
 	code := calculateChallengeCode(event.Request.Session)
-	
+	// sendSMS()
+
 	event.Response = events.CognitoEventUserPoolsCreateAuthChallengeResponse{
 		PrivateChallengeParameters: map[string]string{
 			"code": code,
@@ -61,16 +41,6 @@ func HandleLambdaEvent(ctx context.Context, event *events.CognitoEventUserPoolsC
 		},
 		ChallengeMetadata: code,
 	}
-
-	// e.Response.PublicChallengeParameters = map[string]string{
-	// 	"email": e.Request.UserAttributes["email"],
-	// }
-
-	// e.Response.PrivateChallengeParameters = map[string]string{
-	// 	"secret": secretLoginCode,
-	// }
-
-	// e.Response.ChallengeMetadata = fmt.Sprintf("CODE-%s", secretLoginCode)
 
 	b, _ := json.Marshal(event)
 	log.Println(string(b))
@@ -109,52 +79,4 @@ func calculateChallengeCode(session []*events.CognitoEventUserPoolsChallengeResu
 	return secretLoginCode
 }
 
-// Send -
-func (e *Email) Send() {
-	// Create a new session and specify an AWS Region.
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(SesRegion),
-	})
-
-	// debuging purpose
-	log.Println("check session region, ", *sess.Config.Region)
-
-	// create an SES client in the session.
-	log.Println("create ses client")
-	svc := ses.New(sess)
-
-	// set SES input
-	input := &ses.SendRawEmailInput{
-		Source:       aws.String(e.Sender),
-		Destinations: []*string{aws.String(e.Recipient)},
-		RawMessage:   &ses.RawMessage{Data: []byte(e.MIME)},
-	}
-
-	log.Printf("send to %s\n", e.Recipient)
-	result, err := svc.SendRawEmail(input)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case ses.ErrCodeMessageRejected:
-				fmt.Println(ses.ErrCodeMessageRejected, aerr.Error())
-			case ses.ErrCodeMailFromDomainNotVerifiedException:
-				fmt.Println(ses.ErrCodeMailFromDomainNotVerifiedException, aerr.Error())
-			case ses.ErrCodeConfigurationSetDoesNotExistException:
-				fmt.Println(ses.ErrCodeConfigurationSetDoesNotExistException, aerr.Error())
-			case ses.ErrCodeConfigurationSetSendingPausedException:
-				fmt.Println(ses.ErrCodeConfigurationSetSendingPausedException, aerr.Error())
-			case ses.ErrCodeAccountSendingPausedException:
-				fmt.Println(ses.ErrCodeAccountSendingPausedException, aerr.Error())
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
-		}
-		return
-	}
-
-	fmt.Println(result)
-}
+// func sendSMS()
